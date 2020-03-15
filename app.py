295d402b2bed9e2678d139fbad05d6c75bfe2615
jomari <jomarialang.com>
2020-03-15 02:05:02 +0000

    added host and port to fix the bug in production

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=False)
